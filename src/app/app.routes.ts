912ed19013bf69dccbac9cdb740a12f34664b442
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-06-16 23:21:56 +0200

    Agregado enrutamiento modular para productos y manejo de detalles de producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'products', loadComponent: () => import('./features/products/products.component') },
-  { path: 'product-details/:id', loadComponent: () => import('./features/products/details/details.component') },
-  { path: 'checkout', loadComponent: () => import('./features/checkout/checkout.component') },
+  {
+    path: 'products',
+    loadChildren: () => import('./features/products/products.route')
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./features/checkout/checkout.component')
+  },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: '**', redirectTo: 'products', pathMatch: 'full' }, // Error 404
-
 ];

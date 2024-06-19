8d9e872cab4113ca18747f50b31dede845655931
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-06-19 08:20:13 +0200

    Configuración del Server de Stripe

--- src/app/features/checkout/services/checkout.service.ts
+++ src/app/features/checkout/services/checkout.service.ts
@@ -7,24 +7,22 @@ import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CheckoutService {
-  // private readonly _http = inject(HttpClient);
-  // private readonly _url = environment.serverURL;
+  private readonly _http = inject(HttpClient);
+  private readonly _url = environment.serverURL;
 
-  onProceedToPay(): any{
-
+  onProceedToPay(products: Product[]) {
+    return this._http
+      .post(`${this._url}/checkout`, { items: products })
+      .pipe(
+        map(async (res: any) => {
+          const stripe = await loadStripe(environment.stripeAPIKey);
+          stripe?.redirectToCheckout({ sessionId: res.id });
+        })
+      )
+      .subscribe({
+        error: (err) => console.error('Error', err),
+      });
   }
 
-  // onProceedToPay(products: Product[]) {
-  //   return this._http
-  //     .post(`${this._url}/checkout`, { items: products })
-  //     .pipe(
-  //       map(async (res: any) => {
-  //         const stripe = await loadStripe(environment.stripeAPIKey);
-  //         stripe?.redirectToCheckout({ sessionId: res.id });
-  //       })
-  //     )
-  //     .subscribe({
-  //       error: (err) => console.error('Error', err),
-  //     });
-  // }
+
 }

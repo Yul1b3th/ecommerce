627a084233f4a9af669406c3e4a2d9e0cc67e8f7
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-06-23 13:26:18 +0200

    Auto-generated commit

--- server/chunk-ZREIETWE.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import"./chunk-NDYDZJSS.mjs";var o=[{path:"",loadComponent:()=>import("./chunk-AD3FJKGX.mjs")},{path:":id",loadComponent:()=>import("./chunk-KXRNPWKF.mjs")}],t=o;export{t as default};
